bc71f04c057e34d222774523c4718210a22e11f0
Pandss <Pandeeswari1814@gmail.com>
2022-07-14 15:49:26 +0000

    3rd commit

--- papu.java
+++ papu.java
@@ -5,7 +5,7 @@ public class Main {
 
     for (int i = 1; i <= rows; ++i) {
       for (int j = 1; j <= i; ++j) {
-        System.out.print("* ");
+        System.out.print(j + "  ");
       }
       System.out.println();
     }
